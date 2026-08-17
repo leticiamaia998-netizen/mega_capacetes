fd59fe69bbb09863286d3c3b7a8f3ed2234f710a
Leticia Maia <leticiamaia@gmail.com>
2026-08-17 20:46:05 -0300

    fix: adicionar aliases das rotas admin

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,5 +1,21 @@
 import LegacyStorefront from "../legacy-storefront";
+import { redirect } from "next/navigation";
 
-export default function StorefrontRoute() {
+type StorefrontRouteProps = {
+  params: Promise<{ slug: string[] }>;
+};
+
+export default async function StorefrontRoute({ params }: StorefrontRouteProps) {
+  const { slug } = await params;
+  const pathname = `/${slug.join("/")}`;
+
+  if (pathname === "/admin/login") {
+    redirect("/xxx/login");
+  }
+
+  if (pathname === "/admin") {
+    redirect("/xxx");
+  }
+
   return <LegacyStorefront />;
 }
